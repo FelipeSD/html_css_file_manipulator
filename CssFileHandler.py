b3c3e14d803be7f692d84f99b4acf7b2195aba19
Felipe da Silva Damasceno <felipe98_stz@hotmail.com>
2020-02-18 09:36:03 -0300

    Separação de seletores: COMPLETE
    importância do metodo de inicialização/construtor da classe para cada objeto recomeçar suas propriedades como vazia, foi implementado!

--- CssFileHandler.py
+++ CssFileHandler.py
@@ -1,21 +1,6 @@
 from StyleHandler import StyleHandler
 
 class CSSFileHandler:
-    pseudoClassList = (
-        ":link", ":vistited", ":active", ":hover",
-        ":focus", ":first-child", ":last-child", 
-        ":nth-child", ":nth-last-child", ":nth-of-type", 
-        ":first-of-type", ":last-of-type", ":empty", ":target",
-        ":checked", ":enabled", ":disabled"
-    )
-
-    relationshipSelectors = {
-        " ": "descendent", 
-        ">": "child",
-        "+": "next sibling",
-        "~": "general sibling",
-        "*": "all"
-    }
 
     FullCSSFile = ""
     Styles = [
@@ -26,7 +11,7 @@ class CSSFileHandler:
 
     animation = []
 
-    Statistics = {
+    statistics = {
         "mostUsedSelector": "",
         "mostUsedProperty": ""
     }
@@ -37,7 +22,7 @@ class CSSFileHandler:
 
         ALLFile = self.FullCSSFile.split("\n")
         ALLFile = "".join(ALLFile) #making the file minified
-
+        # print(ALLFile)
         self.handleCSSFile(ALLFile)
 
         CSSFile.close()
@@ -45,15 +30,14 @@ class CSSFileHandler:
     # CSS Files are only handled in minified mode
     def handleCSSFile(self, CSSFile):
         position_start = position_end = 0
-
         while CSSFile.find("{", position_start) > 0 :
             style = StyleHandler()
             position_start = CSSFile.find("{", position_start)
-
-            if(position_end < position_start):
+            
+            if(CSSFile[position_end] == "}"):
                 style.selectors["fullLine"] = CSSFile[position_end+1:position_start] #extracting selectors             
-            else: 
-                style.selectors["fullLine"] = CSSFile[:position_start] #extracting selectors
+            else:
+                style.selectors["fullLine"] = CSSFile[position_end:position_start] #extracting selectors 
 
             position_end = CSSFile.find("}", position_start)
             style.properties["fullLine"] = CSSFile[position_start+1:position_end] # extracting properties
--- StyleHandler.py
+++ StyleHandler.py
@@ -1,38 +1,109 @@
 import re
 
 class StyleHandler:
-    selectors = {
-        "fullLine": "",
-        "classesUsed": [],
-        "idsUsed": [],
-        "pseudoClassesUsed": []
-    }
+    pseudoClassList = (
+        ":link", ":vistited", ":active", ":hover",
+        ":focus", ":first-child", ":last-child", 
+        ":nth-child", ":nth-last-child", ":nth-of-type", 
+        ":first-of-type", ":last-of-type", ":empty", ":target",
+        ":checked", ":enabled", ":disabled"
+    )
 
-    properties = {
-        "fullLine": "",
-        "propetiesArray": []
-    }
+    relationshipSelectors = [
+        " ", 
+        ">",
+        "+",
+        "~",
+        "*"
+    ]
 
-    Statistics = {
-        "selectorsType": {            
-            "tagSelectorsAmount": 0, # when a tag is use to set a style, like 'div', 'a'...
-            "tagAttributeSelectorsAmount": 0, # when an tag attribute is used. Defined between [ ] -> ex: [type="text"]
-            "classSelectorsAmount": 0, # when a class is selected to set a style with a dot '.'
-            "idSelectorsAmount": 0 # when an id tag is used to set the style with a '#'
-        },
-        "pseudoClassesUsed": [], # all pseudo classes commonly used is defined in CssFileHandler
-        "specialPropertiesUsed": [] # @media or @keyframe queries names 
-    }
+    def __init__(self):
+        self.selectors = {
+            "fullLine": "",
+            "classesUsed": [],
+            "idsUsed": [],
+            "tagsUsed": [],
+            "attributesUsed": [],
+            "pseudoClassesUsed": []
+        }
+
+        self.properties = {
+            "fullLine": "",
+            "propetiesArray": []
+        }
+
+        self.statistics = {
+            "selectorsType": {            
+                "tagSelectorsAmount": 0, # when a tag is use to set a style, like 'div', 'a'...
+                "tagAttributeSelectorsAmount": 0, # when an tag attribute is used. Defined between [ ] -> ex: [type="text"]
+                "classSelectorsAmount": 0, # when a class is selected to set a style with a dot '.'
+                "idSelectorsAmount": 0 # when an id tag is used to set the style with a '#'
+            },
+
+            "specialPropertiesUsed": [] # @media or @keyframe queries names 
+        }
 
     def handleSelectors(self):
-        selectors = self.selectors["fullLine"].replace("\t", "") # removing possible existing tabs
-        print(selectors)
+        # self.selectors["classesUsed"] = [],
+        # self.selectors["idsUsed"] = [],
+        # self.selectors["tagsUsed"] = [],
+        # self.selectors["attributesUsed"] = [],
+        # self.selectors["pseudoClassesUsed"] = []
+        
+        selectors = self.selectors["fullLine"].replace(r"\t", "") # removing possible existing tabs
 
-        i = 0
-        while i < len(selectors):
-            print(re.search("(?:.|#|~|+|>|*)", selectors))                
+        selectors = selectors.split(",")
+        # print(selectors)
 
+        for selector in selectors:
+
+            #removing pseudo classes
+            for pseudoClass in self.pseudoClassList:
+                match = re.search(pseudoClass, selector)
+                if(match):
+                    self.selectors["pseudoClassesUsed"].append(pseudoClass)
+                    selector = selector[:match.start()]+selector[match.end():]
+            
+            #removing relationship selectors
+            newSelectorArray = selector.split() #removing descendent selector " " or \s
+            newSelectorArray = [s for s in newSelectorArray if s not in self.relationshipSelectors] #filtering another relationship selectors
+
+            #separating selectors -> ex: .className.anotherClassName
+            for s in newSelectorArray:
+                next_position = 0
+                indexSelector = []
+                while True:
+                    match = re.search(r"\.|\#|\[", s[next_position:])
+                    if(match):
+                        indexSelector.append(match.start())
+                        next_position = match.start()+1
+                    else:
+                        break
+
+                print(indexSelector)
+
+                i=0
+                if(len(indexSelector) != 0 and indexSelector[i] != 0):
+                    print("Primeiro seletor é uma tag")
+                    self.selectors["tagsUsed"].append(s[:indexSelector[i]])
+
+                for i in range(len(indexSelector)):
+                    index = indexSelector[i]
+
+                    if i+1 <= len(indexSelector):
+                        if(s[index] == "."):
+                            print("Class")
+                            self.selectors["classesUsed"].append(s[index:])
+                        elif(s[index] == "#"):
+                            print("ID")
+                            self.selectors["idsUsed"].append(s[index:])
+                        elif(s[index] == "["):
+                            print("ATTRIBUTE")
+                            self.selectors["attributesUsed"].append(s[index:])
+                        else:
+                            print("TAG")
+                            self.selectors["tagsUsed"].append(s[index:])
 
     def handleProperties(self):
         propertiesArray = self.properties["fullLine"].split()
-        print(propertiesArray)
+        # print(propertiesArray)
